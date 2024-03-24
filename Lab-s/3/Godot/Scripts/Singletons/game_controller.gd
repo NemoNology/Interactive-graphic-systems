d44c4f60793a45e8ad5f3e26b723f382e1268a78
NemoNology <chicken.gang@mail.ru>
2024-03-25 05:49:48 +0900

    Game controller: completed

--- Lab-s/3/Godot/Scripts/Singletons/game_controller.gd
+++ Lab-s/3/Godot/Scripts/Singletons/game_controller.gd
@@ -1,1 +1,14 @@
 extends Node
+
+const DEFAULT_IS_HEALTH_ON: bool = true
+const DEFAULT_IS_TIMER_ON: bool = false
+const DEFAULT_HEALTH_POINTS: int = 3
+const DEFAULT_GAME_TIME: int = 60
+const DEFAULT_PAIRS_COUNT: int = 16
+
+var is_health_on: bool = DEFAULT_IS_HEALTH_ON
+var is_timer_on: bool = DEFAULT_IS_TIMER_ON
+var health_points: int = DEFAULT_HEALTH_POINTS
+var game_time: int = DEFAULT_GAME_TIME
+var pairs_count: int = DEFAULT_PAIRS_COUNT
+var is_win: bool = false
