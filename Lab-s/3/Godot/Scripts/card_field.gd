430d9da8fb922a9d28993af46ea46a2bd98cc060
NemoNology <chicken.gang@mail.ru>
2024-03-25 07:11:23 +0900

    Card field
    Added:
    + Completed cards_shufle_and_fill method
    Changed:
    > Now _ready method use cards_shufle_and_fill method

--- Lab-s/3/Godot/Scripts/card_field.gd
+++ Lab-s/3/Godot/Scripts/card_field.gd
@@ -15,17 +15,7 @@ func _init():
 	add_child(box)
 
 func _ready():
-	var cards: Array[Card] = []
-	var card_buffer: Card
-	for i in range(card_pairs_amount):
-		for j in range(2):
-			card_buffer = Card.new()
-			card_buffer.id = i + 1
-			card_buffer.toogled.connect(some_card_toogled)
-			cards.append(card_buffer)
-	cards.shuffle()
-	for card in cards:
-		box.add_child(card)
+	cards_shufle_and_fill()
 
 func some_card_toogled(id: int, is_face_up: bool):
 	card_toogled.emit(id, is_face_up)
@@ -36,10 +26,22 @@ func remove_cards_by_id(id: int):
 			box.remove_child(c)
 
 func cards_shufle_and_fill():
-	var children: Array[Node] = box.get_children()
-	for c in children:
+	# remove possible old cards
+	for c in box.get_children():
 		box.remove_child(c)
-		
+	# fill by new cards
+	var cards: Array[Card] = []
+	var card_buffer: Card
+	for i in range(card_pairs_amount):
+		for j in range(2):
+			card_buffer = Card.new()
+			card_buffer.id = i + 1
+			card_buffer.toogled.connect(some_card_toogled)
+			cards.append(card_buffer)
+	cards.shuffle()
+	for card in cards:
+		box.add_child(card)	
+
 func flip_cards_to(to_state: bool):
 	var cards_amount: int = box.get_child_count() - 1
 	for c: Card in box.get_children() as Array[Card]:
