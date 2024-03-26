4019afa189e8a09561fc87166654f225d89b610c
NemoNology <chicken.gang@mail.ru>
2024-03-26 09:24:53 +0900

    Card field
    Changed:
    > Base class to Container
    > Private methods names: added prefix '_'
    Removed:
    - Extra box (HFlowContainer) child
    - Extra theme constants overridings
    Added:
    + Update cards position method to set position in container for every card
    + Pause possibility before card flip in "flip cards to" method
    + Reset cards method that reset cards amount and shuffle them; then placing them in card field as container

--- Lab-s/3/Godot/Scripts/card_field.gd
+++ Lab-s/3/Godot/Scripts/card_field.gd
@@ -1,49 +1,62 @@
-class_name CardField extends MarginContainer
+class_name CardField extends Container
+
+const CARDS_SEPARATION_LENGTH: int = 8
 
 @export_category("Cards field")
 @export var card_pairs_amount: int = 2
-var box: HFlowContainer = HFlowContainer.new();
 signal card_toogled(id: int, is_face_up: bool)
 
-func _init():
-	box.add_theme_constant_override("h_separation", 8)
-	box.add_theme_constant_override("v_separation", 8)
-	add_theme_constant_override("margin_bottom", 8)
-	add_theme_constant_override("margin_left", 8)
-	add_theme_constant_override("margin_right", 8)
-	add_theme_constant_override("margin_top", 8)
-	add_child(box)
-
 func _ready():
-	cards_shufle_and_fill()
+	reset_cards()
 
-func some_card_toogled(id: int, is_face_up: bool):
+func _some_card_toogled(id: int, is_face_up: bool):
 	card_toogled.emit(id, is_face_up)
 
 func remove_cards_by_id(id: int):
-	for c in box.get_children():
+	for c in get_children():
 		if c is Card and c.id == id:
-			box.remove_child(c)
+			remove_child(c)
 
-func cards_shufle_and_fill():
+func _cards_fill_with_shufle():
 	# remove possible old cards
-	for c in box.get_children():
-		box.remove_child(c)
+	for c in get_children():
+		remove_child(c)
 	# fill by new cards
 	var cards: Array[Card] = []
-	var card_buffer: Card
 	for i in range(card_pairs_amount):
 		for j in range(2):
-			card_buffer = Card.new()
+			var card_buffer: Card = Card.new()
 			card_buffer.id = i + 1
-			card_buffer.toogled.connect(some_card_toogled)
+			card_buffer.toogled.connect(_some_card_toogled)
 			cards.append(card_buffer)
+	# Shuffle cards
 	cards.shuffle()
+	# fill node by cards
 	for card in cards:
-		box.add_child(card)	
+		add_child(card)
 
-func flip_cards_to(to_state: bool):
-	var cards_amount: int = box.get_child_count() - 1
-	for c: Card in box.get_children() as Array[Card]:
+func flip_cards_to(to_state: bool, with_pause: bool = false):
+	var flipping_cards: Array[Card] = []
+	# Determinating what're cards needs to be flipped
+	for c: Card in get_children():
 		if c.is_card_up != to_state:
-			c.toogle(false)
+			flipping_cards.append(c)
+	# Flip cards
+	if with_pause:
+		await get_tree().create_timer(.4).timeout
+	for c: Card in flipping_cards:
+		c.toogle(false)
+
+func _update_cards_positions():
+	var current_width: int = CARDS_SEPARATION_LENGTH
+	var current_height: int = CARDS_SEPARATION_LENGTH
+	for c: Card in get_children():
+		c.position = Vector2(current_width, current_height)
+		current_width += c.size.x + CARDS_SEPARATION_LENGTH
+		if current_width >= size.x:
+			current_width = CARDS_SEPARATION_LENGTH
+			current_height += CARDS_SEPARATION_LENGTH + c.size.y
+
+func reset_cards():
+	_cards_fill_with_shufle()
+	_update_cards_positions()
